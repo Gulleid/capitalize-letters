a063e2f80b22ae631710a879fdac5ce42d13d7ac
Gullied Mohamed <gulleidmohamed93@gmail.com>
2023-06-19 15:09:22 +0300

    capitalise first letter

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Good Luck 💪🏾
+function capitalize(words) {
+    var sword = words.toLowerCase().split(' ');
+    for (var i = 0; i < sword.length; i++) {
+      sword[i] = sword[i].charAt(0).toUpperCase() +
+      sword[i].substring(1);
+    }
+    return sword.join(' ');
+  }
+  console.log(capitalize("my name is ali"));
+  console.log(capitalize("fikrcamp bootcamp"));
+  console.log(capitalize("i live in paris"));
